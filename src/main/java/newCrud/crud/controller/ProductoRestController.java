e9efbad021bc0f70517a595b3965d890d99ce24a
Silvio Rosas <silviorosas@yahoo.com.ar>
2022-04-10 12:43:21 -0300

    creo controlador save para agregar new producto

--- src/main/java/newCrud/crud/controller/ProductoRestController.java
+++ src/main/java/newCrud/crud/controller/ProductoRestController.java
@@ -2,11 +2,16 @@
 package newCrud.crud.controller;
 
 import java.util.List;
+import javax.validation.Valid;
 import newCrud.crud.entidades.Producto;
 import newCrud.crud.repo.ProductoRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +27,11 @@ public class ProductoRestController {
     public List<Producto> getAll(){
         return repo.findAll();
     }
+    
+    @PostMapping(value = "/save")
+	public ResponseEntity<Producto> save(@RequestBody @Valid Producto producto) {
+		Producto obj = repo.save(producto);
+		return new ResponseEntity<Producto>(obj, HttpStatus.OK);
+	}
     
 }
