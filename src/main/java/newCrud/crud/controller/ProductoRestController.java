fac2fe2a89e92f8bafc5b7549528a3246b19c12e
Silvio Rosas <silviorosas@yahoo.com.ar>
2022-04-11 13:07:18 -0300

    creo controlador save para editar y otro para eliminar producto

--- src/main/java/newCrud/crud/controller/ProductoRestController.java
+++ src/main/java/newCrud/crud/controller/ProductoRestController.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +35,18 @@ public class ProductoRestController {
 		Producto obj = repo.save(producto);
 		return new ResponseEntity<Producto>(obj, HttpStatus.OK);
 	}
+        
+        
+	@GetMapping(value = "/delete/{id}")
+	public ResponseEntity<Object> delete(@PathVariable Long id) {
+            repo.deleteById(id);
+            return ResponseEntity.ok(Boolean.TRUE);
+	}
+   
+	}
+       
+        
+
     
-}
+        
+   
